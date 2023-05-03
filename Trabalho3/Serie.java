d6e6e852079900aa66d3a3c8deb6ceb2f172686f
Nathan <72050032+nATHANnSil@users.noreply.github.com>
2023-05-03 15:36:05 -0300

    Update Serie.java

--- Trabalho3/Serie.java
+++ Trabalho3/Serie.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
-/*** - PARTE COMENTADA ESTÁ DESATUALIZADA CONFORME ALTERAÇÕES DO CÓDIGO DOS OUTROS INTEGRANTES DO GRUPO
 
+@SuppressWarnings("unused")
 public class Serie {
 	private String[] GENEROS;
 	private int id_Serie;
@@ -16,10 +18,10 @@ public class Serie {
 	private int quantidadeEpisodios;
 	private int audiencia;
 	private String DataDeLancamento;
-	
-    public Serie() {
-    }
-	
+
+	public Serie() {
+	}
+
 	public Serie(String nome, int id_Serie, String DataDeLancamento, String genero, String idioma,
 			int quantidadeEpisodios, int audiencia) {
 		setNome(nome);
@@ -98,65 +100,60 @@ public class Serie {
 		this.DataDeLancamento = DataDeLancamento;
 
 	}
-	
-***/
-	
-	//Método para salvar serie no arquivo POO_Series.csv
 
-	public void salvar() {
-	    String nomeArquivo =  "POO_Series.csv"; //recebe o nome do arquivo
-	    try (PrintWriter writer = new PrintWriter(new FileWriter(POO_Series.csv))) { //escrever no arquivo 
-	        String dados = id_Serie + "," + nome + "," + DataDeLancamento;
-	        writer.println(dados);
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	}
-
-	// Método para verificar se o ID já está em uso. Se estiver, não salva
-	private boolean idEmUso(int id) {
-	    File diretorio = new File(".");
-	    File[] arquivos = diretorio.listFiles();
-
-	    for (File arquivo : arquivos) {
-	        if (arquivo.isFile() && arquivo.getName().startsWith("serie_")) {
-	            try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
-	                String linha;
-	                while ((linha = reader.readLine()) != null) {
-	                    if (linha.startsWith(String.valueOf(id))) {
-	                        return true;
-	                    }
-	                }
-	            } catch (IOException e) {
-	                e.printStackTrace();
+	boolean idEmUso(int id) {
+	    String nomeArquivo = "POO_Series.csv";
+	    try (Scanner scanner = new Scanner(new File(nomeArquivo))) {
+	        while (scanner.hasNextLine()) {
+	            String linha = scanner.nextLine();
+	            String[] dados = linha.split(";");
+	            if (dados.length > 0 && dados[0].equals(Integer.toString(id))) {
+	                return true;
 	            }
 	        }
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
 	    }
-
 	    return false;
 	}
 
-    // Método para verificar se o nome já está em uso, se estiver não salva.
-	private boolean nomeEmUso(String nome) {
-        File diretorio = new File(".");
-        File[] arquivos = diretorio.listFiles();
+	@SuppressWarnings("deprecation")
+	public void salvar() {
+	    Scanner scanner = new Scanner(System.in);
+	    String nomeArquivo = "POO_Series.csv";
 
-        for (File arquivo : arquivos) {
-            if (arquivo.isFile() && arquivo.getName().startsWith("serie_")) {
-                try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
-                    String linha;
-                    while ((linha = reader.readLine()) != null) {
-                        String[] dados = linha.split("\t");
-                        if (dados.length >= 2 && dados[1].trim().equalsIgnoreCase(nome)) {
-                            return true;
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+	    while (true) {
+	        System.out.println("Digite o ID da série (ou 'PARE' para finalizar):");
+	        String idStr = scanner.nextLine();
 
-        return false;
-    }
+	        if (idStr.equalsIgnoreCase("pare")) {
+	            break;
+	        }
+
+	        try {
+	            int id = Integer.parseInt(idStr);
+
+	            if (idEmUso(id)) {
+	                System.out.println("ID já em uso. Não é possível salvar a série.");
+	            } else {
+	                System.out.println("Digite o nome da série:");
+	                String nome = scanner.nextLine();
+
+	                System.out.println("Digite a data de lançamento da série:");
+	                String dataDeLancamento = scanner.nextLine();
+
+	                try (PrintWriter writer = new PrintWriter(new FileWriter(nomeArquivo, true))) {
+	                    writer.println(id + ";" + nome + ";" + dataDeLancamento);
+	                    System.out.println("Série salva com sucesso!");
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        } catch (NumberFormatException e) {
+	            System.out.println("ID inválido!");
+	        }
+	    }
+
+	    scanner.close();
+	}
 }
